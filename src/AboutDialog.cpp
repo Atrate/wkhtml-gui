3f5476ea6506449142fa5390a80221977219de1e
Atrate <Atrate@protonmail.com>
2020-10-26 19:13:49 +0100

    [Licensing] Add copyright(left) notice [skip ci]
    Signed-off-by: Atrate <Atrate@protonmail.com>

--- src/AboutDialog.cpp
+++ src/AboutDialog.cpp
@@ -1,7 +1,21 @@
+/*
+ * Copyright (C) 2020 Atrate <atrate@protonmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 3 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ */
+
 #include "AboutDialog.h"
 #include "ui_AboutDialog.h"
 
-AboutDialog::AboutDialog(QWidget *parent) :
+AboutDialog::AboutDialog(QWidget* parent) :
     QDialog(parent),
     ui(new Ui::AboutDialog)
 {
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,22 @@
+/*
+ * Copyright (C) 2020 Atrate <atrate@protonmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 3 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ */
+
 #include "MainWindow.h"
 
 #include <QApplication>
 
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
